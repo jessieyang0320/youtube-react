68b4ec914d15ac4caf2d34ea9a1b161421d46a54
Jessie Yang <jessieyang@Jessies-MacBook-Air.local>
2017-09-22 23:47:32 -0400

    fetch data from youtube and setState

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 
@@ -7,17 +7,34 @@ import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyCbqBUuIrWH74oikNsxj35eOPJ2YNT5h3A';
 
-YTSearch({key: API_KEY, term:'hello'}, function(data){
-	console.log(data);
-})
 
-const App = ()=> {
-	return(
-		<div>
 
-			<SearchBar />
-		</div>
+class App extends Component {
+
+	constructor(props){
+		super(props);
+
+		this.state = { 
+			videos: [] 
+		};
+
+		YTSearch({key: API_KEY, term:'surfboard'}, (videos)=>{
+	       this.setState({ videos });
+	       // ES6 syntax sugar for ({videos: videos}), only works when key and variable name are the same
+        });
+	}
+
+	render(){
+
+		return(
+				<div>
+
+					<SearchBar />
+				</div>
 		)
+
+	}
+	
 }
 
 ReactDOM.render(<App/>, document.querySelector('.container'));
