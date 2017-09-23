d0af01efa29b5028a1b1f07d9271b1a59788e709
Jessie Yang <jessieyang@Jessies-MacBook-Air.local>
2017-09-22 23:35:40 -0400

    add component structure files, add search bar component and use API key to fetch data from youtube

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import YTSearch from 'youtube-api-search';
 
-import App from './components/app';
-import reducers from './reducers';
+import SearchBar from './components/search_bar';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+const API_KEY = 'AIzaSyCbqBUuIrWH74oikNsxj35eOPJ2YNT5h3A';
+
+YTSearch({key: API_KEY, term:'hello'}, function(data){
+	console.log(data);
+})
+
+const App = ()=> {
+	return(
+		<div>
+
+			<SearchBar />
+		</div>
+		)
+}
+
+ReactDOM.render(<App/>, document.querySelector('.container'));
